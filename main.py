1162fd9db32118e24d91267dc6e31567077290ac
mayagat2002 <mayagat2002@gmail.com>
2025-11-25 17:02:49 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 # Add the import statements for functions from string_utils.py and equation_utils.py here
 
-from string_utils.py import parse_chemical_reaction 
-from string_utils.py import count_atoms_in_reaction 
-from equation_utils.py import build_equations
+
+from string_utils import parse_chemical_reaction
+from string_utils import count_atoms_in_reaction
+from equation_utils import build_equations
+from equation_utils import my_solve
 
 def split_at_number (formula):
     digit_location = 1
